2de1867f7668fe6720ad0664f18c6ba0f3a24d80
Puneeth <79633135+darkxdd@users.noreply.github.com>
2023-09-22 05:19:54 +0000

    Add files via upload

--- timetable.py
+++ timetable.py
@@ -1,14 +1,34 @@
-faculty_list = ["John", "Alice", "Bob", "Eve"]
-days_list = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-hours_list = ["8:00 AM", "10:00 AM", "1:00 PM", "3:00 PM"]
-subjects_list = ["Math", "Science", "English", "History"]
 
-print("-" * 50)
-print(f"{'| Faculty':<10}{'| Days':<12}{'| Hours':<10}{'| Subjects':<15} |")
-print("-" * 50)  
-for faculty in faculty_list:
-    for day in days_list:
-        for hour in hours_list:
-            for subject in subjects_list:
-                print(f"{'| ' + faculty:<10}{'| ' + day:<12}{'| ' + hour:<10}{'| ' + subject:<15} |")
-                print("-" * 50)  
+schedule = {
+    'Faculty 1': {
+        'Monday': {
+            '9:00 AM - 10:00 AM': 'Subject A',
+            '10:00 AM - 11:00 AM': 'Subject B',
+            '11:00 AM - 12:00 PM': 'Subject C',
+        },
+        'Tuesday': {
+            '9:00 AM - 10:00 AM': 'Subject D',
+            '10:00 AM - 11:00 AM': 'Subject E',
+            '11:00 AM - 12:00 PM': 'Subject F',
+        },
+    },
+    'Faculty 2': {
+        'Monday': {
+            '9:00 AM - 10:00 AM': 'Subject X',
+            '10:00 AM - 11:00 AM': 'Subject Y',
+            '11:00 AM - 12:00 PM': 'Subject Z',
+        },
+        'Tuesday': {
+            '9:00 AM - 10:00 AM': 'Subject P',
+            '10:00 AM - 11:00 AM': 'Subject Q',
+            '11:00 AM - 12:00 PM': 'Subject R',
+        },
+    },
+}
+
+
+print("Faculty\t\tDay\t\tTime\t\tSubject")
+for faculty, faculty_schedule in schedule.items():
+    for day, day_schedule in faculty_schedule.items():
+        for time, subject in day_schedule.items():
+            print(f"{faculty}\t{day}\t{time}\t{subject}")
